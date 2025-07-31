69bf16ad10c6b4cefcce5ff4bc02cea0122b0c30
Adolfo Rosa <adolfoklk0307@gmail.com>
2025-07-31 00:41:26 -0400

    Haciendo ejercicio 2

---
+++ ejercicios/ejercicio2.ts
@@ -0,0 +1,50 @@
+class Banco {
+  private colaClientes: string[] = []
+
+  encolarCliente(cliente: string) {
+    this.colaClientes.push(cliente)
+    console.log(`Cliente "${cliente}" ha llegado y está en la cola.`)
+  }
+
+  atenderCliente() {
+    if (this.colaClientes.length === 0)
+      return console.log('No hay clientes para atender.')
+
+    const clienteAtendido = this.colaClientes.shift()
+    console.log(`Atendiendo al cliente: ${clienteAtendido}`)
+  }
+
+  mostrarCola() {
+    if (this.colaClientes.length === 0)
+      return console.log('Cola de clientes vacía.')
+
+    console.log('Clientes en espera:')
+    for (const cliente of this.colaClientes) console.log(`- ${cliente}`)
+  }
+}
+
+// Ejemplo de uso
+const banco = new Banco()
+
+banco.encolarCliente('Carlos')
+banco.encolarCliente('Ana')
+banco.encolarCliente('Luis')
+console.log()
+
+banco.mostrarCola()
+console.log()
+
+banco.atenderCliente()
+banco.mostrarCola()
+console.log()
+
+banco.atenderCliente()
+banco.mostrarCola()
+console.log()
+
+banco.atenderCliente()
+banco.mostrarCola()
+console.log()
+
+banco.atenderCliente()
+console.log()
